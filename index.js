789438910ac9b25fba14403a54c17d122729b1fd
jirishus <jirishus@yahoo.com>
2018-12-16 18:46:22 -0800

    webhook post route

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+var bodyParser = require('body-parser');
+
+
 const app = express();
 const PORT = 3000;
 
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/hello', function (req, res) {
   res.send('Hello World')
 });
 
-app.get('/emailPayload', function (req, res) {
+app.post('/emailPayload', function (req, res) {
   console.log('receiving payload');
   console.log('-------------------');
   res.send('receiveding pauyload');
@@ -16,3 +25,17 @@ app.get('/emailPayload', function (req, res) {
  
 app.listen(PORT);
 console.log(`App started on port: ${ PORT }`);
+
+
+ 
+// POST /login gets urlencoded bodies
+// app.post('/login', urlencodedParser, function (req, res) {
+//   if (!req.body) return res.sendStatus(400)
+//   res.send('welcome, ' + req.body.username)
+// })
+ 
+// POST /api/users gets JSON bodies
+// app.post('/api/users', jsonParser, function (req, res) {
+//   if (!req.body) return res.sendStatus(400)
+//   // create user in req.body
+// })
